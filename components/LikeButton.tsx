f1cd3222d9b79f1aafcac919f0ebefa9c9a14e1a
Anurag <anuragambekar1997@gmail.com>
2023-12-05 21:12:29 +0530

    like song functionality

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -5,6 +5,7 @@ import { useUser } from "@/hooks/useUser"
 import { useSessionContext } from "@supabase/auth-helpers-react"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"
 
 interface LikeButtonProps{
@@ -33,10 +34,35 @@ const LikeButton:React.FC<LikeButtonProps> = ({songId}) => {
 
     const Icon = liked ? AiFillHeart : AiOutlineHeart
 
+    const handleLike = async () =>{
+        if(!user) return authModal.onOpen()
+        if(liked){
+            const {error} = await supabaseClient.from('liked_songs').delete().eq('user_id',user.id).eq('song_id',songId)
+
+            if(error) toast.error(error.message)
+            else setLiked(false)
+        }else{
+            const {error} = await supabaseClient.from('liked_songs').insert({'user_id':user.id,'song_id':songId})
+            if(error)toast.error(error.message)
+            else {
+                setLiked(true)
+                toast.success('Liked!')
+            }
+        }
+        router.refresh()
+    }
+
   return (
-   <button>
-    <Icon color={liked?'#22c55e':'white'}/>
-   </button>
+   <button 
+      className="
+        cursor-pointer 
+        hover:opacity-75 
+        transition
+      "
+      onClick={handleLike}
+    >
+      <Icon color={liked ? '#22c55e' : 'white'} size={25} />
+    </button>
   )
 }
 

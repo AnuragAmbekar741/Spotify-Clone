8ba1a375904a3078abb9268c2a20d855663f3624
Anurag <anuragambekar1997@gmail.com>
2023-12-09 18:51:06 +0530

    player3

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import useGetSongById from '@/hooks/useGetSongById'
 import usePlayer from '@/hooks/usePlayer'
 import React from 'react'
 
-const player = usePlayer()
-
 const Player = () => {
+    const player = usePlayer()
+    const {song} = useGetSongById(player.activeId)
+    // const {songUrl} = useGetSongById
   return (
     <div>Player</div>
   )

7afb91c819c12abeb56efabe369ffa981005141d
Anurag <anuragambekar1997@gmail.com>
2023-12-11 23:28:32 +0530

    play song from liked,search,home

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -3,6 +3,7 @@
 import { Song } from "@/types"
 import MediaItem from "@/components/MediaItem"
 import LikeButton from "@/components/LikeButton"
+import useOnPlay from "@/hooks/useOnPlay"
 
 interface SearchContentProps{
     songs:Song[]
@@ -10,6 +11,7 @@ interface SearchContentProps{
 
 const SearchContent:React.FC<SearchContentProps> = ({songs}) => {
 
+  const onPlay = useOnPlay(songs)
     if(songs.length === 0){
         return (
             <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
@@ -27,7 +29,7 @@ const SearchContent:React.FC<SearchContentProps> = ({songs}) => {
         >
           <div className="flex-1">
             <MediaItem 
-              onClick={(id: string) => {}} 
+              onClick={(id: string) => onPlay(id)} 
               data={song}
             />
           </div>
